a8659de93ab60a58986dc426dd6bf12d7b6bb233
zhaofan10086 <2834059459@qq.com>
2020-10-28 22:09:21 +0800

    zf2209

--- project-provider/src/main/java/com/jk/ProjectProviderApplication.java
+++ project-provider/src/main/java/com/jk/ProjectProviderApplication.java
@@ -3,11 +3,13 @@ package com.jk;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(value="com.jk.mapper")
+@RefreshScope
 public class ProjectProviderApplication {
 
 	public static void main(String[] args) {
--- project-provider/src/main/java/com/jk/controller/HelloController.java
+++ project-provider/src/main/java/com/jk/controller/HelloController.java
@@ -1,14 +1,27 @@
 package com.jk.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
         return "HelloWorld";
     }
+
+    @RequestMapping("/port")
+    @ResponseBody
+    public String port(){
+        System.out.println(port);
+        return port;
+    }
+
 }
